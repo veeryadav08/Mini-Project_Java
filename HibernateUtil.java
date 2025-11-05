e0e85c68867acf2c7017a7d4333b580edd96a51c
veeryadav08 <veermehta4321@gmail.com>
2025-11-05 19:04:15 +0530

    HibernateUtil.java

---
+++ HibernateUtil.java
@@ -0,0 +1,28 @@
+package com.example.hibernatecrud;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory factory;
+
+    static {
+        try {
+            factory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(Student.class)
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("SessionFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return factory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
